cbd2881dc27d5be24c62960044eb69cfcbf2644b
Aaditya Sharma <accolite-asharma@evernote.com>
2020-10-26 18:08:56 +0530

    Added MvcExceptionHandler

--- src/main/java/aaditya/dev/cafeapp/web/controller/v2/BeerControllerV2.java
+++ src/main/java/aaditya/dev/cafeapp/web/controller/v2/BeerControllerV2.java
@@ -1,18 +1,15 @@
 package aaditya.dev.cafeapp.web.controller.v2;
 
-import aaditya.dev.cafeapp.web.model.v2.BeerDtoV2;
 import aaditya.dev.cafeapp.services.v2.BeerServiceV2;
+import aaditya.dev.cafeapp.web.model.v2.BeerDtoV2;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Validated
@@ -51,18 +48,5 @@ public class BeerControllerV2 {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBeer(@PathVariable("beerId") UUID beerId) {
         beerServiceV2.deleteById(beerId);
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<List> validateErrorHandler(ConstraintViolationException e) {
-        List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
-
-        e.getConstraintViolations().forEach(constraintViolation -> {
-            errors.add(constraintViolation.getPropertyPath()
-                    + " : "
-                    + constraintViolation.getMessage());
-        });
-
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
